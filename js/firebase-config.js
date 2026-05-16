c699d2d419f1a367228838fdaff3145fe4144826
João Pedro Rodrigues da Silva <joao.p.silva443@aluno.senai.br>
2026-05-16 09:38:53 -0300

    thirt commit

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,16 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  projectId: "controle-financeiro-pj",
-  appId: "1:114231766796:web:51665865380ce98af9e329",
-  storageBucket: "controle-financeiro-pj.firebasestorage.app",
   apiKey: "AIzaSyCZjH-Nkz8HLRLMVzND_5ZqTerHs2od1xo",
   authDomain: "controle-financeiro-pj.firebaseapp.com",
+  projectId: "controle-financeiro-pj",
+  storageBucket: "controle-financeiro-pj.firebasestorage.app",
   messagingSenderId: "114231766796",
-  projectNumber: "114231766796"
+  appId: "1:114231766796:web:51665865380ce98af9e329"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finai-cache-v2';
+const CACHE_NAME = 'finai-cache-v3';
 const urlsToCache = [
     './',
     './index.html',
